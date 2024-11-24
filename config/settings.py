e5e45ce2728ac191a1c8dab3630cf9b9ee659ae8
ghost160111 <jalol100.ghost@gmail.com>
2024-11-24 16:46:50 +0500

    returned original password

--- config/settings.py
+++ config/settings.py
@@ -66,7 +66,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'mirrors',
         'USER': 'postgres',
-        'PASSWORD': 'Zukhriddin12',
+        'PASSWORD': 'dimatop12',
         'HOST': 'localhost',
         'PORT': '5432'
     }
